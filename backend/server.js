c00b7594ed0659a63ecdba88a4eeb4724fa36c12
Dvir Cohen <linoymicheal@gmail.com>
2024-06-13 20:59:23 +0300

    backend deploy

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const http=require('http');
-const port= 8000;
-const app = require('./app')
 
+const app = require('./app')
+const port= 8000;
 const server=http.createServer(app); 
 
-server.listen(port);
+
+const PORT=process.env.PORT||3000;
+server.listen(PORT,()=> console.log("server is runing on port 3000"));
