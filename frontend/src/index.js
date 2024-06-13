ce43b3703a899e6f8b9d13fc51f1336e4c3719ce
Dvir Cohen <linoymicheal@gmail.com>
2024-06-13 20:10:02 +0300

    ready to deploy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,9 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
+if(process.env.NODE_ENV==='production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -3,5 +3,6 @@ import listReducer from "../features/listSlice"
 
 export const store = configureStore({
     reducer: listReducer,
+    devTools:false
 
 })
